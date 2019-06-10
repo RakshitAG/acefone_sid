d675fa6a72d14c5ae1de15ddf7ab7101b7f38493
rakshit.agarwal <rakshit.agarwal@myrealdata.net>
2019-06-10 11:47:00 +0530

    first commit

---
+++ src/SessionServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace iMi\LaravelTransSid;
+
+class SessionServiceProvider extends \Illuminate\Session\SessionServiceProvider
+{
+    public function register()
+    {
+
+            $this->registerSessionManager();
+
+            $this->registerSessionDriver();
+
+            $this->app->singleton('Illuminate\Session\Middleware\StartSession', 'iMi\LaravelTransSid\StartSessionMiddleware');
+
+    }
+}
